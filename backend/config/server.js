404503042951ae42449b8e5063c1815003dec339
Anang H. Darmawan <anang.hendri@clozette.co>
2021-04-11 10:25:27 +0700

    feature: backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b550ac6cea055747068ddbe03574451d'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17b64d6c-e51a-4443-930b-aad79d861136'
+};
